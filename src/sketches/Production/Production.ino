28378c89a460fb2e344c749b6fc06134f642980a
Bastian Raschke <bastian.raschke@posteo.de>
2015-02-03 22:52:59 +0100

    Re-implemented button trigger instead of stupid capacitive sensor

--- src/sketches/Production/Production.ino
+++ src/sketches/Production/Production.ino
@@ -183,75 +183,6 @@ bool generateOneTimePad(char result[ARDUKEY_OTP_SIZE])
     return true;
 }
 
-uint8_t readCapacitivePin(int pinToMeasure)
-{
-    // Variable used to translate from Arduino to AVR pin naming
-    volatile uint8_t* port;
-    volatile uint8_t* ddr;
-    volatile uint8_t* pin;
-
-    // Here we translate the input pin number from Arduino pin number to the AVR PORT, PIN, DDR
-    // and which bit of those registers we care about.
-    byte bitmask;
-    port = portOutputRegister(digitalPinToPort(pinToMeasure));
-    ddr = portModeRegister(digitalPinToPort(pinToMeasure));
-    bitmask = digitalPinToBitMask(pinToMeasure);
-    pin = portInputRegister(digitalPinToPort(pinToMeasure));
-
-    // Discharge the pin first by setting it low and output
-    *port &= ~(bitmask);
-    *ddr  |= bitmask;
-
-    delay(1);
-
-    // Back up the AVR Status Register
-    uint8_t SREG_old = SREG;
-
-    // Prevent the timer IRQ from disturbing our measurement
-    noInterrupts();
-
-    // Make the pin an input with the internal pull-up on
-    *ddr &= ~(bitmask);
-    *port |= bitmask;
-
-    // Now see how long the pin to get pulled up. This manual unrolling of the loop
-    // decreases the number of hardware cycles between each read of the pin,
-    // thus increasing sensitivity.
-    uint8_t cycles = 17;
-
-         if (*pin & bitmask) { cycles =  0;}
-    else if (*pin & bitmask) { cycles =  1;}
-    else if (*pin & bitmask) { cycles =  2;}
-    else if (*pin & bitmask) { cycles =  3;}
-    else if (*pin & bitmask) { cycles =  4;}
-    else if (*pin & bitmask) { cycles =  5;}
-    else if (*pin & bitmask) { cycles =  6;}
-    else if (*pin & bitmask) { cycles =  7;}
-    else if (*pin & bitmask) { cycles =  8;}
-    else if (*pin & bitmask) { cycles =  9;}
-    else if (*pin & bitmask) { cycles = 10;}
-    else if (*pin & bitmask) { cycles = 11;}
-    else if (*pin & bitmask) { cycles = 12;}
-    else if (*pin & bitmask) { cycles = 13;}
-    else if (*pin & bitmask) { cycles = 14;}
-    else if (*pin & bitmask) { cycles = 15;}
-    else if (*pin & bitmask) { cycles = 16;}
-
-    // End of timing-critical section;
-    // turn interrupts back on if they were on before, or leave them off if they were off before
-    SREG = SREG_old;
-
-    // Discharge the pin again by setting it low and output
-
-    // Note: It's important to leave the pins low if you want to be able to
-    // touch more than 1 sensor at a time - if the sensor is left pulled high,
-    // when you touch two sensors, your body will transfer the charge between sensors.
-    *port &= ~(bitmask);
-    *ddr  |= bitmask;
-
-    return cycles;
-}
-
 /*
  * The Arduino setup function.
  *
@@ -266,9 +197,15 @@ void setup()
     #endif
 
     initializeArduKey();
+
+    // Configure button pin as an input with internal pull-up resistor
+    pinMode(ARDUKEY_PIN_BUTTON, INPUT_PULLUP);
+
+    // Additionally set pin default state
+    digitalWrite(ARDUKEY_PIN_BUTTON, HIGH);
 }
 
-int previousCapacitiveState = HIGH;
+int previousButtonState = HIGH;
 
 /*
  * The Arduino loop function.
@@ -284,11 +221,11 @@ void loop()
 
     char otp[ARDUKEY_OTP_SIZE] = "";
 
-    // Get current capacitive state
-    int capacitiveState = readCapacitivePin(ARDUKEY_PIN_CAPACITIVE_SENSOR);
+    // Get current button state
+    int buttonState = digitalRead(ARDUKEY_PIN_BUTTON);
 
-    // Check if capacitive state changed (polling)
-    if ( capacitiveState > 1 && previousCapacitiveState == 1 )
+    // Check if button state changed (polling)
+    if ( buttonState != previousButtonState && buttonState == LOW )
     {
         generateOneTimePad(otp);
 
@@ -308,5 +245,5 @@ void loop()
         #endif
     }
 
-    previousCapacitiveState = capacitiveState;
+    previousButtonState = buttonState;
 }

65f7dcd3ad84eef2143f319c7f0634375f679f72
Bastian Raschke <bastian.raschke@posteo.de>
2015-06-24 12:16:51 +0200

    Updated generate-keys.py

--- Development/Generate AES and secret keys/generate-keys.py
+++ Development/Generate AES and secret keys/generate-keys.py
@@ -12,12 +12,10 @@ else:
 rawString = ''
 arrayOutput = ''
 
-## Generate random nonce
 for _ in range(length):
     c = random.SystemRandom().randint(0, 255)
     rawString = rawString + str(hex(c))[2:]
     arrayOutput = arrayOutput + hex(c) + ', '
-
 
 print('Raw string:  ' + rawString)
 print('Array style: ' + arrayOutput)
